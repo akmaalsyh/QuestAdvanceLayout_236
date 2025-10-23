03d86d19a75616fe275b040b514b314d67787aff
akmaalsyh <taufansyah50@gmail.com>
2025-10-23 08:21:45 +0700

    memasukan fungsi ActivitasPertama dan menambah teks prodi

--- app/src/main/java/com/example/questadvancelayout_236/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_236/UIPertama.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -19,6 +20,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -26,4 +28,13 @@ import androidx.compose.ui.unit.sp
 import com.example.questadvancelayout_236.R
 
 @Composable
-fun Activit
+fun ActivitasPertama(modifier: Modifier) {
+    Column(modifier = Modifier.padding(top=100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally)
+        {
+            Text(text = stringResource(id = R.string.prodi),
+                fontSize = 35.sp,
+                fontWeight = FontWeight.Bold)
+        }
+}
