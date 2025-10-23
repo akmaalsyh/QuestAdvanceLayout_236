78a9bec0ffd06f09b0d18da48c0e8f5228ca9ad5
akmaalsyh <taufansyah50@gmail.com>
2025-10-23 08:24:48 +0700

    menambah spasi diantara 2 text diatas dengan box card nya

--- app/src/main/java/com/example/questadvancelayout_236/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_236/UIPertama.kt
@@ -38,6 +38,7 @@ fun ActivitasPertama(modifier: Modifier) {
                 fontWeight = FontWeight.Bold)
             Text(text = stringResource(id = R.string.univ),
                 fontSize = 22.sp)
+            Spacer(modifier = Modifier.height(height = 25.dp))
 
         }
 }
