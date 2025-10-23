174bd3550504ff946b77805185519e345788bb06
akmaalsyh <taufansyah50@gmail.com>
2025-10-23 09:29:45 +0700

    mengubah warna nama dan alamatnya

--- app/src/main/java/com/example/questadvancelayout_236/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_236/UIPertama.kt
@@ -54,13 +54,13 @@ fun ActivitasPertama(modifier: Modifier) {
                             text = stringResource(id = R.string.nama),
                             fontSize = 30.sp,
                             fontFamily = FontFamily.Cursive,
-                            color = Color.White,
+                            color = Color.Black,
                             modifier = Modifier.padding(top = 15.dp)
                         )
                         Text(
                             text = stringResource(id = R.string.alamat),
                             fontSize = 20.sp,
-                            color = Color.Yellow,
+                            color = Color.Blue,
                             modifier = Modifier.padding(top=10.dp)
                         )
                     }
