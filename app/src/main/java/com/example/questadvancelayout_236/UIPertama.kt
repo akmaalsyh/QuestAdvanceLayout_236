7f16b32b28c9dddc0e306d7bcfcc0741b5ba91b0
akmaalsyh <taufansyah50@gmail.com>
2025-10-23 09:03:09 +0700

    menghapus library import yg tidak digunakan

--- app/src/main/java/com/example/questadvancelayout_236/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_236/UIPertama.kt
@@ -18,15 +18,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.AlignmentLine
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.questadvancelayout_236.R
 
 @Composable
 fun ActivitasPertama(modifier: Modifier) {
